2bb80b4403d1384c62be8cdb64db9e58be2bfd62
Piotr Grzybowski <grzybowski_p@wp.pl>
2023-09-06 11:32:11 +0200

    work

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "App.module.css";
+import styles from "./App.module.css";
 import { Form } from "./components/Form/Form";
 import { Header } from "./components/Header/Header";
 import { Item } from "./components/Item/item";
